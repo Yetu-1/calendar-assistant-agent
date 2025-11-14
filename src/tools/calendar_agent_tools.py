29b4c0b1736e17d46c5c5fed28371891da3d5119
David-Salihu <davidsalihu19@gmail.com>
2025-11-14 17:14:44 +0100

    Implement reschedule events in calendar tool

--- src/tools/calendar_agent_tools.py
+++ src/tools/calendar_agent_tools.py
@@ -53,8 +53,22 @@ def fetch_events(time_min: EventDateTime, time_max: EventDateTime) -> str:
     events = events_list.get("items", [])
     return f"Events:\n {events}"
 
+def patch_event(event_id: str, start: EventDateTime, end: EventDateTime) -> str:
+    client = CalendarClient();
+    result = client.service.events().patch(
+            calendarId=Settings.CALENDAR_ID,
+            eventId=event_id,
+            body={
+                "start": start.model_dump(),  # Converts Pydantic model to dict
+                "end": end.model_dump(),   
+            }
+    ).execute()
+    return f"Result: {result}"
+
+
 get_datetime_tool = FunctionTool(get_date_and_time, description="Use this tool to fetch current date and time.")
 add_event_to_calendar_tool = FunctionTool(
     add_event_to_calendar, description="Use to add event to calendar."
 )
 fetch_events_tool = FunctionTool(fetch_events, description="Use this tool to fetch events from the calendar.")
+reschedule_event_tool = FunctionTool(patch_event, description="Use the tool to reschedule and update event in the calendar.")
